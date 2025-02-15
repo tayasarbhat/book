173bcb192f214e11ee99fbb745cab579834070ab
tayasarbhat <tayasarbhat@gmail.com>
2025-02-15 20:32:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/book/', // Replace 'your-repo-name' with the actual repository name
+  base: '/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
 });
